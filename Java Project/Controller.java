e4d7440d2eadb6d6d0712457dcfef05147160c75
Afia Tasnim <77075953+afia25@users.noreply.github.com>
2025-09-29 11:30:26 +0600

    Delete Java Project directory

--- Java Project/Controller.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-import java.io.Serializable;
-
-public class Controller {
-
-}
-
--- Java Project/InvalidSalaryException.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import java.io.Serializable;
-
-public class InvalidSalaryException extends Exception implements Serializable {
-    public InvalidSalaryException(String message) {
-        super(message);
-    }
-}
